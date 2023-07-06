ecb06038731abafcca8e89bcc348cc3cd63661eb
Anton <imladris.doc@gmail.com>
2023-07-06 17:56:28 +0300

    News Fragment worked

--- app/src/main/java/com/example/main/NewsAdapter.kt
+++ app/src/main/java/com/example/main/NewsAdapter.kt
@@ -3,6 +3,8 @@ package com.example.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -14,37 +16,42 @@ import com.example.notebets.models.AppNote
 import com.example.notebets.models.FaqModel
 import com.example.notebets.models.News
 import com.example.notebets.ui.home.HomeFragment
+import com.squareup.picasso.Picasso
 
-class NewsAdapter: ListAdapter<News, NewsAdapter.Holder>(Comparator()){
-    class Holder(view: View) : RecyclerView.ViewHolder(view){
-        val binding = ItemNewsBinding.bind(view)
+class NewsAdapter(private val mList: List<News>) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-        fun bind (item: News) = with(binding){
-//            tvDetails.text = item.descr
-//            textName.text = item.name
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_news, parent, false)
 
-        }
+        return ViewHolder(view)
     }
 
-    class Comparator : DiffUtil.ItemCallback<News>(){
-        override fun areItemsTheSame(oldItem: News, newItem: News): Boolean {
-            return  oldItem == newItem
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        }
+        val ItemsViewModel = mList[position]
 
-        override fun areContentsTheSame(oldItem: News, newItem: News): Boolean {
-            return oldItem == newItem
-        }
+
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = ItemsViewModel.text
+        holder.textViewName.text = ItemsViewModel.name
+        Picasso.get().load(ItemsViewModel.image).into(holder.imageView)
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
-        return Holder(view)
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
     }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bind(getItem(position))
-
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view_news)
+        val textView: TextView = itemView.findViewById(R.id.tv_details_news)
+        val textViewName: TextView = itemView.findViewById(R.id.textName_news)
     }
 }
--- app/src/main/java/com/example/notebets/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/notebets/ui/notifications/NotificationsFragment.kt
@@ -1,15 +1,28 @@
 package com.example.notebets.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.main.MainAdapter
+import com.example.main.NewsAdapter
+import com.example.notebets.R
 import com.example.notebets.databinding.FragmentNotificationsBinding
+import com.example.notebets.models.News
+import com.example.retrofit.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class NotificationsFragment : Fragment() {
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: NewsAdapter
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -34,6 +47,20 @@ class NotificationsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        binding.recyclerview.layoutManager = LinearLayoutManager(activity)
+
+        val apiInterface = ApiInterface.create().getMovies()
+
+        apiInterface.enqueue( object : Callback<List<News>>{
+            override fun onResponse(call: Call<List<News>>?, response: Response<List<News>>?) {
+                val adapter = NewsAdapter(response?.body()!!)
+                binding.recyclerview.adapter = adapter
+            }
+            override fun onFailure(call: Call<List<News>>?, t: Throwable?) {
+
+            }
+        })
 
     }
 
